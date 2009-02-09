53045d533db03a1edd9f8385b1b9336c0a871870
sfentress <sfentress@6e01202a-0783-4428-890a-84243c50cc2b>
2009-02-09 18:38:42 +0000

    Allow customization of main graph toolbar buttons (visibility, order). First button passed will be default (selected) button.
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/data/datagraph@17010 6e01202a-0783-4428-890a-84243c50cc2b

--- src/org/concord/datagraph/ui/DataGraphToolbar.java
+++ src/org/concord/datagraph/ui/DataGraphToolbar.java
@@ -51,35 +51,91 @@ public class DataGraphToolbar extends GraphWindowToolBar
 {
 	private static final long serialVersionUID = 1L;
 	protected AbstractButton selButton;
+
+	public final static int SELECT_BTN = 0;
+	public final static int ZOOM_IN_BTN = 1;
+	public final static int ZOOM_OUT_BTN = 2;
+	public final static int RESTORE_SCALE_BTN = 3;
 	
     public DataGraphToolbar()
     {
         super(false);
         
-		selButton = addButton("arrow.gif", 
-		        "" + MultiRegionAxisScale.DRAGMODE_TRANSLATE_DILATE, 
-		        "Move and Scale graph");
-
-		addButton("zoomin.gif", 
-		        "" + AxisScale.DRAGMODE_ZOOM_IN, 
-		        "Zoom in to a point");
-
-		addButton("zoomout.gif", 
-		        "" + AxisScale.DRAGMODE_ZOOM_OUT, 
-		        "Zoom out from a point");
-		
-		addButton("restorescale.gif", "restorescale", "Restore initial scale", false);
+		selButton = addButton(SELECT_BTN);
+		addButton(ZOOM_IN_BTN);
+		addButton(ZOOM_OUT_BTN);
+		addButton(RESTORE_SCALE_BTN);
 
 		setDefaultButton(selButton);
 		
     }
     
+    /**
+     * Creates a tool bar with the buttons specified. The buttons will be
+     * added in the order specified, and the first button added will be the
+     * default button, which is selected when the graph is first opened.
+     * 
+     * @param buttons
+     */
+    public DataGraphToolbar(int[] buttons){
+    	super(false);
+    	
+    	for (int i = 0; i < buttons.length; i++) {
+	        AbstractButton button = addButton(buttons[i]);
+	        if (i == 0 && button != null){
+	        	setDefaultButton(button);
+	        }
+        }
+    	
+    }
+    
+    /**
+     * Adds button of the specified type and returns the newly
+     * created button.
+     * 
+     * @param buttonType
+     * @return
+     */
+    public AbstractButton addButton(int buttonType){
+    	AbstractButton button = null;
+    	switch (buttonType){
+    		case SELECT_BTN:
+    			button = addButton("arrow.gif", 
+    			        "" + MultiRegionAxisScale.DRAGMODE_TRANSLATE_DILATE, 
+    			        "Move and Scale graph");
+    			selButton = button;
+    			break;
+    		case ZOOM_IN_BTN:
+    			button = addButton("zoomin.gif", 
+        		        "" + AxisScale.DRAGMODE_ZOOM_IN, 
+        		        "Zoom in to a point");
+    			break;
+    		case ZOOM_OUT_BTN:
+    			button = addButton("zoomout.gif", 
+        		        "" + AxisScale.DRAGMODE_ZOOM_OUT, 
+        		        "Zoom out from a point");
+    			break;
+    		case RESTORE_SCALE_BTN:
+    			button = addButton("restorescale.gif", 
+    					"restorescale", "Restore initial scale", false);
+    			break;
+    		default:
+    			System.err.println("No button of that tye is defined");
+    	}
+    	return button;
+    }
+    
 	/**
+	 * This is called when graph is first viewed, and is a good time to
+	 * select the default button.
+	 * 
 	 * @see org.concord.graph.examples.GraphWindowToolBar#addAxisScale(org.concord.graph.engine.AxisScale)
 	 */
 	public void addAxisScale(AxisScale ax)
 	{
 		super.addAxisScale(ax);
-		selButton.doClick();
+		if (getDefaultButton() != null){
+			getDefaultButton().doClick();
+		}
 	}
 }
