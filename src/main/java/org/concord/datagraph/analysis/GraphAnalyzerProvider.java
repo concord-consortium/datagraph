99b8eb67aeb06765ff3f1636f553dd185a300666
aunger <aunger@6e01202a-0783-4428-890a-84243c50cc2b>
2011-05-05 16:59:52 +0000

    Remove extra annotation.
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/data/datagraph@24564 6e01202a-0783-4428-890a-84243c50cc2b

--- src/main/java/org/concord/datagraph/analysis/GraphAnalyzerProvider.java
+++ src/main/java/org/concord/datagraph/analysis/GraphAnalyzerProvider.java
@@ -8,7 +8,6 @@ public abstract class GraphAnalyzerProvider {
     public static enum Type { ANY, NETLOGO }
     public abstract GraphAnalyzer getAnalyzer(Type preferredType);
     
-    @SuppressWarnings("restriction")
     public static GraphAnalyzer findAnalyzer(Type preferredType) {
         // ServiceRegistry actually just wraps sun.misc.Service.providers(), and doesn't require compiler tweaks.
         // If we ever move to 1.6, this should get changed to use java.util.ServiceLoader
