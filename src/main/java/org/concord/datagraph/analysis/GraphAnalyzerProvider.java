084ad4b0fb1b8b63c839c22119d19000ec350132
aunger <aunger@6e01202a-0783-4428-890a-84243c50cc2b>
2011-05-05 16:36:56 +0000

    Remove direct reference to internal sun classes.
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/data/datagraph@24562 6e01202a-0783-4428-890a-84243c50cc2b

--- src/main/java/org/concord/datagraph/analysis/GraphAnalyzerProvider.java
+++ src/main/java/org/concord/datagraph/analysis/GraphAnalyzerProvider.java
@@ -2,13 +2,17 @@ package org.concord.datagraph.analysis;
 
 import java.util.Iterator;
 
+import javax.imageio.spi.ServiceRegistry;
+
 public abstract class GraphAnalyzerProvider {
     public static enum Type { ANY, NETLOGO }
     public abstract GraphAnalyzer getAnalyzer(Type preferredType);
     
     @SuppressWarnings("restriction")
     public static GraphAnalyzer findAnalyzer(Type preferredType) {
-        Iterator<?> providers = sun.misc.Service.providers(GraphAnalyzerProvider.class);
+        // ServiceRegistry actually just wraps sun.misc.Service.providers(), and doesn't require compiler tweaks.
+        // If we ever move to 1.6, this should get changed to use java.util.ServiceLoader
+        Iterator<?> providers = ServiceRegistry.lookupProviders(GraphAnalyzerProvider.class);
         while (providers.hasNext()) {
             GraphAnalyzerProvider gas = (GraphAnalyzerProvider)providers.next();
             GraphAnalyzer ga = gas.getAnalyzer(Type.ANY);
