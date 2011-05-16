24dc4c9515d29a642d5fbcd3cf7ce7f5f93bb59a
aunger <aunger@6e01202a-0783-4428-890a-84243c50cc2b>
2011-05-16 16:28:50 +0000

    GraphAnalyzers should now be able to annotate the graph with the analysis results.
    git-svn-id: https://svn.concord.org/svn/projects/trunk/common/java/data/datagraph@24656 6e01202a-0783-4428-890a-84243c50cc2b

--- src/main/java/org/concord/datagraph/analysis/GraphAnalyzer.java
+++ src/main/java/org/concord/datagraph/analysis/GraphAnalyzer.java
@@ -1,11 +1,14 @@
 package org.concord.datagraph.analysis;
 
 import java.awt.Component;
+import java.util.ArrayList;
 
 import org.concord.data.state.OTDataStore;
 import org.concord.datagraph.analysis.rubric.GraphRubric;
 import org.concord.datagraph.analysis.rubric.ResultSet;
+import org.concord.datagraph.state.OTDataCollector;
 import org.concord.framework.otrunk.OTObjectList;
+import org.concord.graph.util.state.OTHideableAnnotation;
 
 public interface GraphAnalyzer {
     public Graph getSegments(OTDataStore dataStore, int xChannel, int yChannel, double tolerance) throws IndexOutOfBoundsException;
@@ -13,4 +16,5 @@ public interface GraphAnalyzer {
     public ResultSet compareGraphs(GraphRubric expected, Graph received);
     public String getHtmlReasons(ResultSet results);
     public void displayHtmlReasonsPopup(Component parent, ResultSet results);
+    public ArrayList<OTHideableAnnotation> annotateResults(OTDataCollector studentObject, ResultSet scoreResults);
 }
